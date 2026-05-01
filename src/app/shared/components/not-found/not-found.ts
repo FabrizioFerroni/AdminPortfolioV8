443a3262f2db81a72e7adaa22804fffc29addb8a
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2026-04-30 23:33:29 -0300

    chore: create notfound component

--- src/app/shared/components/not-found/not-found.ts
+++ src/app/shared/components/not-found/not-found.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { ZardButtonComponent } from '../button';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideArrowLeft, lucideHome } from '@ng-icons/lucide';
+import { Rutas } from '@/shared/utils';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [RouterLink, ZardButtonComponent, NgIcon],
   templateUrl: './not-found.html',
   styleUrl: './not-found.css',
+  viewProviders: [
+    provideIcons({
+      lucideHome,
+      lucideArrowLeft,
+    }),
+  ],
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+  dashboardUrl = `/${Rutas.DASHBOARD}`;
+  volverAtras() {
+    window.history.back();
+  }
+}
