d35950d28dd9dae523c19c2c21c632fcf499fb91
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2026-04-30 22:59:42 -0300

    refactor: se quita codigo comentado

--- src/app/shared/services/layout.ts
+++ src/app/shared/services/layout.ts
@@ -6,29 +6,6 @@ import { map } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class LayoutService {
-  /*private breakpointObserver = inject(BreakpointObserver);
-
-  private isMobile = toSignal(
-    this.breakpointObserver
-      .observe([Breakpoints.Handset, Breakpoints.Tablet])
-      .pipe(map(r => r.matches)),
-    { initialValue: false }
-  );
-
-  // Estado manual del usuario (toggle)
-  private _collapsed = signal(false);
-
-  // Si es mobile, siempre collapsed. Si no, respeta el toggle manual
-  readonly sidebarCollapsed = computed(() => this.isMobile() || this._collapsed());
-
-  toggle() {
-    this._collapsed.update(v => !v);
-  }
-
-  setCollapsed(value: boolean) {
-    this._collapsed.set(value);
-  }*/
-
   private breakpointObserver = inject(BreakpointObserver);
 
   isMobile = toSignal(
@@ -38,13 +15,11 @@ export class LayoutService {
     { initialValue: false }
   );
 
-  // Se inicializa según el dispositivo actual
   private _collapsed = signal(this.isMobile());
 
   readonly sidebarCollapsed = computed(() => this._collapsed());
 
   constructor() {
-    // Cuando cambia el tamaño de pantalla, resetea el estado
     effect(() => {
       this._collapsed.set(this.isMobile());
     });
